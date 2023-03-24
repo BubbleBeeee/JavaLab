09f1abb3d25769f901837fe33c5d7e6200609410
BubbleBeeee <bubblebeeee@outlook.com>
2023-03-24 22:04:18 +0100

    Dokończenie funkcji kalkulatora

--- src/lab02/Kalkulator.java
+++ src/lab02/Kalkulator.java
@@ -15,19 +15,22 @@ public class Kalkulator {
 	
 	public static void Menu() {
 		System.out.println(
-				"\t\t\t--------------------\n" +
-						"\t\t\t---- Kalkulator ----\n" +
-						"\t\t\t--------------------\n"
+				"""
+						\t\t\t--------------------
+						\t\t\t---- Kalkulator ----
+						\t\t\t--------------------
+						"""
 		);
 		System.out.println(
-				"\t\t\t1. Suma\n" +
-						"\t\t\t2. Różnica\n" +
-						"\t\t\t3. Iloczyn\n" +
-						"\t\t\t4. Iloraz\n" +
-						"\t\t\t5. Potęga\n" +
-						"\t\t\t6. Pierwiastek\n" +
-						"\t\t\t7. Funkcja Trygonometryczna\n" +
-						"\t\t\t8. Wyjście"
+				"""
+						\t\t\t1. Suma
+						\t\t\t2. Różnica
+						\t\t\t3. Iloczyn
+						\t\t\t4. Iloraz
+						\t\t\t5. Potęga
+						\t\t\t6. Pierwiastek
+						\t\t\t7. Funkcja Trygonometryczna
+						\t\t\t8. Wyjście"""
 		);
 		
 		int choice = InInt();
@@ -50,37 +53,77 @@ public class Kalkulator {
 		return scanner.nextInt();
 	}
 	
-	public static String InString() {
-		Scanner scanner = new Scanner(System.in);
-		return scanner.nextLine();
-	}
-	
 	public static void Add() {
-	
+		System.out.println("Podaj liczby do dodania: ");
+		int a = InInt();
+		int b = InInt();
+		System.out.println(a + " + " + b + " = " + (a+b));
 	}
 	
 	public static void Sub() {
-	
+		System.out.println("Podaj liczby do odjęcia: ");
+		int a = InInt();
+		int b = InInt();
+		System.out.println(a + " - " + b + " = " + (a-b));
 	}
 	
 	public static void Mul() {
-	
+		System.out.println("Podaj liczby do pomnożenia: ");
+		int a = InInt();
+		int b = InInt();
+		System.out.println(a + " * " + b + " = " + (a*b));
 	}
 	
 	public static void Div() {
-	
+		System.out.println("Podaj licznik: ");
+		int a = InInt();
+		
+		System.out.println("Podaj mianownik: ");
+		int b = InInt();
+		
+		System.out.println(a + " / " + b + " = " + (float)a/b);
 	}
 	
 	public static void Exp() {
-	
+		System.out.println("Podaj podstawę: ");
+		int a = InInt();
+		
+		System.out.println("Podaj wykładnik: ");
+		int b = InInt();
+		
+		System.out.println(a + " ^ " + b + " = " + Math.pow(a, b));
 	}
 	
 	public static void Root() {
-	
+		System.out.println("Podaj liczbę do spierwiastkowania: ");
+		int a = InInt();
+		
+		System.out.println("Podaj stopień pierwiastka: ");
+		int b = InInt();
+		
+		System.out.println(a + " ^ 1/" + b + " = " + Math.pow(a, 1.0/b));
 	}
 	
 	public static void Trig() {
-	
+		System.out.println("Podaj miarę kąta w stopniach: ");
+		double phi = InInt() / 360.0 * 2 * Math.PI;
+		
+		System.out.println("""
+				Wybierz funkcję:
+				1. Sin
+				2. Cos
+				3. Tan
+				4. Ctg""");
+		
+		int choice = InInt();
+		
+		switch (choice) {
+			case 1 -> System.out.println(Math.sin(phi));
+			case 2 -> System.out.println(Math.cos(phi));
+			case 3 -> System.out.println(Math.tan(phi));
+			case 4 -> System.out.println(1.0 / Math.tan(phi));
+			default -> System.out.println("Nieprawidłowy wybór!");
+		}
 	}
 	
 	public static void Close() {
