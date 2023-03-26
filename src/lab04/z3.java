46850d1f9ea7395e6c6e3bef245265a3a8384085
BubbleBeeee <bubblebeeee@outlook.com>
2023-03-26 12:50:38 +0200

    Lab 4 Zadanie 3

---
+++ src/lab04/z3.java
@@ -0,0 +1,14 @@
+package lab04;
+
+public class z3 {
+	public static void main(String[] args) {
+		Run();
+	}
+	
+	public static void Run() {
+		String[] strings = {"Abc", "asfvSdafgFa", "seBastian", "defghij"};
+		
+		for (String s : strings)
+			System.out.println(s.toUpperCase());
+	}
+}
